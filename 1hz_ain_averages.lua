7d08bbd2fc0e0466952bdfd01772215bac4d09ba
stenslae <141681972+stenslae@users.noreply.github.com>
2023-08-08 12:00:27 -0600

    1hz_ain_averages.lua

--- 1hz_ain_averages.lua
+++ 1hz_ain_averages.lua
@@ -13,7 +13,6 @@ local ljWrite = MB.W
 local ljRead = MB.R
 local ljnameToAddress = MB.nameToAddress
 
-
 -- Sampling interval, 1 per second
 local intrvlhz = 1
 local intrvlms = math.floor(1/intrvlhz * 1000)
@@ -42,7 +41,7 @@ end
 
 --Set resindex to 12 for AIN0-AIN3
 for i=0,3 do
-  ljWrite(((ljnameToAddress"AIN0_RESOLUTION_INDEX")+(i)), 12) 
+  ljWrite(((ljnameToAddress"AIN0_RESOLUTION_INDEX")+(i)), 0, 12) 
 end
 
 -- Configure an interval
@@ -54,7 +53,7 @@ while true do
   if LJ.CheckInterval(0) then
     -- Read the AIN channels
     for i=1,numchannels-1 do
-      ain[i][index] = ljRead(ljnameToAddress("AIN"..(i-1)))
+      ain[i][index] = ljRead(ljnameToAddress("AIN"..(i-1)), 3)
     end
     
     --If 10 readings have been made for each channel
@@ -67,7 +66,7 @@ while true do
         end
         ainavg[i] = sums[i] / numsamp
         -- Save result to USER_RAM#_F32 register
-        ljWrite((46000 + (channels[i]-1)*2), ainavg[i])
+        ljWrite((46000 + (channels[i])), 3, ainavg[i])
       end
       --Prepare the array for new reading by shifting the array over and adjusting index.
       for i=1, numchannels-1 do
