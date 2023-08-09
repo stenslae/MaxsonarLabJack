e0e7cee93c6ca33e24609ae7023142e3d71c0c12
stenslae <141681972+stenslae@users.noreply.github.com>
2023-08-09 16:12:49 -0600

    emails.py

--- emails.py
+++ emails.py
@@ -1,15 +1,19 @@
-#for this program to run, you need python and sengrid account and the api key set up.
-#this needs to be located in python folder for it to be called by cmp + usable with lv program
-
+#for this program to run, you need python and sengrid account and the api key should be set up in your os environment
+#see: https://labjack.com/pages/support/?doc=/app-notes/networking/sending-emails-from-a-program/#header-three-pojeq
+#this needs to be located in python folder for it to be called by command prompt + usable with lv program
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+#collect user input for email details
 message = Mail(
     from_email=  input('Insert From Email: '),
     to_emails= input('Insert To Email: '),
     subject= input('Insert Subject: '),
-    html_content= input('Insert Message: '),)
+    html_content= input('Insert Message: '),
+    api_key= input('Provide your API Key: '),)
+
+#send the email
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
