38edea574cde3a07dba1d18508580dd0f48ef094
stenslae <141681972+stenslae@users.noreply.github.com>
2023-08-11 11:17:38 -0600

    Add files via upload

--- threshold_and_average.lua
+++ threshold_and_average.lua
@@ -22,7 +22,6 @@
   local top = 1
   local bottom = 1
 
-
 --Local functions for faster processing
 local ljWrite = MB.W
 local ljRead = MB.R
@@ -30,7 +29,6 @@ local ljnameToAddress = MB.nameToAddress
 
 
 --Initialize variables
-local ainavg = {}
 local sums = {}
 local index = 1
 local ain = {}
@@ -53,47 +51,51 @@ ljWrite(ljnameToAddress("AIN_ALL_RESOLUTION_INDEX"), 0, resindx)
 -- Configure an interval
 LJ.IntervalConfig(0, intrvlms)
 
--- Begin loop
-while true do
-  -- Execute loop every intrvlms.
-  if LJ.CheckInterval(0) then
-      -- Read the AIN channels
-    for i=1,numchannels do 
-      ain[i][index]= ljRead(ljnameToAddress("AIN"..(i-1)), 3)
-    end
 
-    -- Execute if numscans or more scans have been made
-    if index==numscans then
-      --Cut off data from the top and/or bottom based on the threshold percentage
-      for i =1,numchannels do
-        table.sort(ain[i])  --- FIX SORTING PROCESS, IT DOESNT WORK
-        for j=1, numscans*thresh do
-          if top == 1 then
-            table.remove(ain[i], index) 
-            index = index -1 
+if (top == 0 or top == 1) and (bottom == 0 or bottom == 1) then
+  -- Begin loop
+  while true do
+    -- Execute loop every intrvlms.
+    if LJ.CheckInterval(0) then
+        -- Read the AIN channels
+      for i=1,numchannels do 
+        ain[i][index]= ljRead(ljnameToAddress("AIN"..(i-1)), 3)
+      end
+  
+      -- Execute if numscans or more scans have been made
+      if index==numscans then
+        --Cut off data from the top and/or bottom based on the threshold percentage
+        for i =1,numchannels do
+          table.sort(ain[i])
+          for j=1, numscans*thresh do
+            if top == 1 then
+              table.remove(ain[i])
+            end
+            if bottom == 1 then
+              table.remove(ain[i], 1) 
+            end
           end
-          if bottom == 1 then
-            table.remove(ain[i], 1) 
-            index = index -1 
+        end
+        
+        index = index -thresh*numscans*top -numscans*thresh*bottom
+        
+        --Find the average of each channel's current readings
+        local ainavg = {}
+        for i=1, numchannels do
+          sums[i] = 0
+          for j=1,index do
+            sums[i] = sums[i] + ain[i][j]
           end
+          ainavg[i] = sums[i] / index
+          -- Save result to USER_RAM#_F32 register
+          ljWrite((46000 + ((i-1)*2)), 3, ainavg[i])
         end
       end
       
-      --Find the average of each channel's current readings
-      for i=1, numchannels do
-        sums[i] = 0
-        for j=1,index do
-          sums[i] = sums[i] + ain[i][j]
-        end
-        ainavg[i] = sums[i] / index
-        -- Save result to USER_RAM#_F32 register
-        ljWrite((46000 + ((i-1)*2)), 3, ainavg[i])
-      end
+      --Save number of scans to user ram
+      ljWrite((46000 + ((numchannels)*2)), 3, count)
+      index = index +1
+      count = count + 1 
     end
-    
-    --Save number of scans to user ram
-    ljWrite((46000 + ((numchannels)*2)), 3, count)
-    index = index +1
-    count = count + 1 
-  end
-end 
+  end 
+else print("Only 1 and 0 are valid values for top and bottom.") end
