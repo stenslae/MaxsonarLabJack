fbb17cec17966789e585e16d7e463a9f6a63ef91
stenslae <141681972+stenslae@users.noreply.github.com>
2024-06-07 09:35:26 -0600

    Update c++_maxsonar_sensor_example_code.c

--- c++_maxsonar_sensor_example_code.c
+++ c++_maxsonar_sensor_example_code.c
@@ -1,54 +1,90 @@
-#The following is made for the T7/T7-Pro and is most effective when no condensation is expected on the sensor.
-#The sensor has a range of 300mm to 5000mm
-#Expected connections: Sensor - FIO0, AIN3, OBA(DAC0), GND
-import math
-import time
-from labjack import ljm
+//The following is made for the T7/T7-Pro and is most effective when no condensation is expected on the sensor.
+//The sensor has a range of 300mm to 5000mm
+//Expected connections: Sensor - FIO0, AIN3, OBA(DAC0), GND
 
-#The following value can be set to a range of 2.7 V to 5.0 V.
-vsupply = 3.3
+#include <stdio.h>
+#include <LabJackM.h> 
+#include "LJM_Utilities.h" 
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <cmath>
+#include <string>
 
-#Find LabJack device
-handle = ljm.openS("ANY", "USB", "ANY")
+int main() { 
+	//The following value can be set to a range of 2.7 V to 5.0 V.
+	double vsupply = 3.3;
 
-#Initialize variables
-pwm = 0
-ain = 0
-name = ["DIO_EF_CLOCK0_ENABLE", "DIO_EF_CLOCK1_ENABLE", "DIO_EF_CLOCK2_ENABLE", "DIO_EF_CLOCK0_DIVISOR",
-            "DIO_EF_CLOCK0_OPTIONS", "DIO_EF_CLOCK0_ROLL_VALUE", "DIO0_EF_ENABLE", "DAC0"]
-num = [0, 0, 0, 1,
-           0, 0, 0, vsupply]
-numFrames = len(name)
 
-#Powers sensor and makes sure all settings are default.
-ljm.eWriteNames(handle, numFrames, name, num)
-print('Measuring...')
+	int err, handle; 
+	double value = 0; 
+	const char * NAME = {"SERIAL_NUMBER"}; 
 
-#The following sets up the DIO_EF to measure PWM output
-clockName = ["DIO0_EF_INDEX", "DIO_EF_CLOCK0_ENABLE", "DIO0_EF_ENABLE"]
-clockNum = [5, 1, 1]
-numFrames = len(clockName)
-ljm.eWriteNames(handle, numFrames, clockName, clockNum)
+	//Find LabJack device
+	err = LJM_Open(LJM_dtANY, LJM_ctANY, "LJM_idANY", &handle);
+	ErrorCheck(err, "LJM_Open");
 
-#Give time to ensure sensor can get a reading.
-time.sleep(8)
+	//Initialize variables
+	double pwm = 0;
+	double ain = 0;
 
-#Measures ToF of sonar in seconds.
-pwm = ljm.eReadName(handle, "DIO0_EF_READ_A_F_AND_RESET")
-print(pwm)
-#Converts to distance.
-pwmf = math.trunc(pwm*1000*3.28084*1000)/1000
-pwmm = math.trunc(pwm*1000*1000)/1000
-#Print results for viewer
-print('The PWM output measured a distance of: ', pwmf, ' feet and ', pwmm, ' meters.')
+	//Initialize variables
+	std::vector<std::string> names = {"DIO_EF_CLOCK0_ENABLE", "DIO_EF_CLOCK1_ENABLE", "DIO_EF_CLOCK2_ENABLE", "DIO_EF_CLOCK0_DIVISOR", "DIO_EF_CLOCK0_OPTIONS", "DIO_EF_CLOCK0_ROLL_VALUE", "DIO0_EF_ENABLE", "DAC0"};
+	std::vector<double> nums = {0, 0, 0, 1, 0, 0, 0, vsupply};
+	int numFrames = names.size();
 
-#Measures the ain output.
-ain = ljm.eReadName(handle, "AIN3")
-#Converts to distance.
-ainf = math.trunc(ain*((5.120*3.28084)/vsupply)*1000)/1000
-ainm = math.trun(ain*(5.120/vsupply)*1000)/1000
-#Print results for viewer
-print('The analog output measured a distance of: ', ainf, ' feet and ', ainm, ' meters.')
+	// Convert names to char* array for LJM_eWriteNames
+	std::vector<const char*> namePtrs;
+	for (const auto& name : names) {
+		namePtrs.push_back(name.c_str());
+    	}
 
-#The program ends.
-ljm.close(handle)
+	//Powers sensor and makes sure all settings are default.
+	err = LJM_eWriteNames(handle, numFrames, name, num);
+	ErrorCheck(err, "LJM_eWriteNames");
+	printf("Measuring...");
+
+	//The following sets up the DIO_EF to measure PWM output
+	std::vector<std::string> clockNames = {"DIO0_EF_INDEX", "DIO_EF_CLOCK0_ENABLE", "DIO0_EF_ENABLE"};
+	std::vector<double> clockNums = {5, 1, 1};
+	numFrames = clockNames.size();
+
+	// Convert clockNames to char* array for LJM_eWriteNames
+	std::vector<const char*> clockNamePtrs;
+	for (const auto& name : clockNames) {
+		clockNamePtrs.push_back(name.c_str());
+	}
+
+	err = LJM_eWriteNames(handle, numFrames, clockName, clockNum);
+	ErrorCheck(err, "LJM_eWriteNames");
+
+	//Give time to get a sensor reading
+	std::this_thread::sleep_for(std::chrono::seconds(8));
+
+	//Measures ToF of sonar in seconds.
+	err = LJM_eReadName(handle, "DIO0_EF_READ_A_F_AND_RESET", &pwm);
+	ErrorCheck(err, "LJM_eReadName");
+	printf("The ToF is %f seconds.\n", pwm);
+
+	//Converts to distance.
+	pwmf = round(pwm*1000*3.28084*1000)/1000;
+	pwmm = round(pwm*1000*1000)/1000;
+	//Print results for viewer
+	printf("The PWM output measured a distance of: %f feet and %f meters.\n", pwmf, pwmm);
+
+	//Measures the ain output.
+	err = LJM_eReadName(handle, "AIN3", &ain);
+	ErrorCheck(err, "LJM_eReadName");
+
+	//Converts to distance.
+	double ainf = round(ain*((5.120*3.28084)/vsupply)*1000)/1000;
+	double ainm = round(ain*(5.120/vsupply)*1000)/1000;
+	//Print results for viewer
+	printf("The analog output measured a distance of: %f feet and %f meters.\n", ainf, ainm);
+
+	// Close device handle 
+	err = LJM_Close(handle); 
+	ErrorCheck(err, "LJM_Close"); 
+	return LJME_NOERROR; 
+
+}
